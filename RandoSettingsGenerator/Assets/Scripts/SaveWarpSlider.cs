1133b964bd542463fea8d8e19339d05c71494f40
JasterRogue <ryanmadigan141@gmail.com>
2022-07-06 22:17:59 +0100

    - UI started
    Signed-off-by: JasterRogue <ryanmadigan141@gmail.com>

---
+++ RandoSettingsGenerator/Assets/Scripts/SaveWarpSlider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveWarpSlider : MonoBehaviour
+{
+    [SerializeField]private Slider warpSlider;
+    [SerializeField] private Text currentValue = null;
+    private List<string> valueList = new List<string> { "Only Warp from Save Points", "Warp out of Combat", "No Restriction Warp"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        warpSlider.onValueChanged.AddListener(delegate { SliderValueChangedCallback(); });
+
+        // assuring that our slider is setup properly to map values
+        warpSlider.minValue = 0;
+        warpSlider.maxValue = valueList.Count - 1;
+        warpSlider.wholeNumbers = true;
+        warpSlider.value = 2;
+    }
+
+    private void SliderValueChangedCallback()
+    {
+        // grab out numeric value of the slider - cast to int as the value should be a whole number
+        int numericSliderValue = (int)warpSlider.value;
+
+        // debugging - do whatever you want with this value
+        currentValue.text = valueList[numericSliderValue];
+    }
+
+    public int getWarpSliderValue()
+    {
+        return (int)warpSlider.value;
+    }
+}
